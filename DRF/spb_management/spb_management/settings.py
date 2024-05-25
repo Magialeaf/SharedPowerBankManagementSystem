c1c6b68936dbe28594d68892a4e1e078d6bef0a8
Magialeaf <1132358898@qq.com>
2024-05-25 22:04:10 +0800

    Update settings.py

--- DRF/spb_management/spb_management/settings.py
+++ DRF/spb_management/spb_management/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mzlf_w#-&o-i$&optx83sarx@6=(4o6##mnm@c+v+x+xv_fmux'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
